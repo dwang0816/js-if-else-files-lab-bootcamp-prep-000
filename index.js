132ede03deb3330f667caa203ff9ba62f52dd0a2
dennis wang <dwang0816@gmail.com>
2019-03-27 19:51:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ var height = "74"
 } else {
     console.log("Expected the name to be Susan")
 }
+ if (height = "74") {
+   console.log("the height is correct")
+ } else {
+    console.log("Expected the height to be 74")
+ }
 // Don't worry about this
 module.exports = { name, height
 }
